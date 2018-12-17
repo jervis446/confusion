07d2a824d46c3f18aff2adc6df1cecb6e0fa2c34
jervis446 <kumar.adarshluv99@gmail.com>
2018-12-17 22:52:43 +0530

    some changes

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand herf="/">Ristorante Con Fusion</NavbarBrand>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
-          </Navbar>
+        </Navbar>
       </div>
     );
   }
--- confusion/src/index.js
+++ confusion/src/index.js
@@ -11,11 +11,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App)
