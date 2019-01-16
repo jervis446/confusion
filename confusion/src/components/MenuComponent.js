d40ca28edbeb4c500651b84b5b9a00a236157470
jervis446 <kumar.adarshluv99@gmail.com>
2019-01-16 21:48:04 +0530

    Single Page Applications Part 2

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
-import { Card, CardImg, CardTitle, CardImgOverlay } from 'reactstrap'
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
-function RenderMenuItem ({dish,onClick}){
+function RenderMenuItem ({dish, onClick}) {
     return (
-        <Card key={dish.id} >
-             <CardImg width="100%" src={dish.image} alt={dish.name}/>
+        <Card>
+            <Link to={`/menu/${dish.id}`} >
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
@@ -25,10 +28,20 @@ const Menu = (props) => {
         return (
             <div className="container">
                 <div className="row">
-                   {menu}
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    {menu}
                 </div>
             </div>
         );
-      }
+}
 
 export default Menu;
