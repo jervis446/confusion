6e63f97209de6029463a92ed6ca9efe65f9b3f5c
jervis446 <kumar.adarshluv99@gmail.com>
2019-01-16 21:29:26 +0530

    first spa app

--- confusion/src/components/MainComponents.js
+++ confusion/src/components/MainComponents.js
@@ -5,14 +5,23 @@ import DishDetail from './DishdetailComponents';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -21,12 +30,28 @@ class Main extends Component {
   }
 
   render() {
+
+
+     const HomePage = () => {
+      return(
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
+    }
+
     return (
       <div>
-         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-        <Footer />
+        <Header/>
+         <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />} />
+              <Redirect to='home' />
+          </Switch>
+          <Footer/>
       </div>
     );
   }
