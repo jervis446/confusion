977b07f3b443e56318d6878524282ee7ced50e5a
jervis446 <kumar.adarshluv99@gmail.com>
2019-01-17 12:47:49 +0530

    Assignment

--- confusion/src/components/MainComponents.js
+++ confusion/src/components/MainComponents.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+
+import About from './AboutComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponents';
-import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+
+import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -54,10 +57,11 @@ class Main extends Component {
         <Header/>
          <Switch>
               <Route path='/home' component={HomePage} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />} />
-              <Redirect to='home' />
+              <Route exact path='/contactus' component={Contact} />
+              <Redirect to="./home" />
           </Switch>
           <Footer/>
       </div>
